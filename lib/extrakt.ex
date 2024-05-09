ab176656e66608baa38af509fbc9052db007e311
Adam Erikuly <adamerikoff@protonmail.com>
2024-05-09 22:29:50 +0500

    init II

---
+++ lib/extrakt.ex
@@ -0,0 +1,18 @@
+defmodule Extrakt do
+  @moduledoc """
+  Documentation for `Extrakt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Extrakt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
